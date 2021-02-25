5ce9e13d36dcf815acd6ec7648457394e46bdd74
Haggets <avilacarrerro@gmail.com>
2021-02-25 00:45:31 -0400

    Corrected Palm bone layer

--- props.py
+++ props.py
@@ -6,7 +6,7 @@ class VAT_properties(bpy.types.PropertyGroup): #Defines global properties the pl
     #Thanks to Jeacom for this
     def armature_poll(self, object):
         #Generated armatures should not be part of the list
-        if object.name.endswith(".weight") or object.name.endswith(".anim"):
+        if object.name.endswith('.weight') or object.name.endswith('.anim') or object.name.endswith('.anim_setup'):
             pass
         else:
             return object.type == 'ARMATURE'
@@ -15,13 +15,13 @@ class VAT_properties(bpy.types.PropertyGroup): #Defines global properties the pl
     def object_poll(self, object):
         return object.type == 'MESH'
         
-    target_armature : bpy.props.PointerProperty(type=bpy.types.Object, name="Armature", description="Armature that will be used to perform operations on", poll=armature_poll, update=functions.create_armature)
+    target_armature : bpy.props.PointerProperty(type=bpy.types.Object, name='Armature', description="Armature that will be used to perform operations on", poll=armature_poll, update=functions.create_armature)
     
-    target_object : bpy.props.PointerProperty(type=bpy.types.Object, name="Object", description="Object linked to the armature that will be used for shapekeys", poll=object_poll)
+    target_object : bpy.props.PointerProperty(type=bpy.types.Object, name='Object', description="Object linked to the armature that will be used for shapekeys", poll=object_poll)
 
     custom_scheme_enabled : bpy.props.BoolProperty(name="Enable custom prefix", description="If to allow usage of custom prefixes that will replace the default Source prefixes", default=0) 
 
-    custom_scheme_prefix : bpy.props.StringProperty(name="Prefix", description="Custom prefix that will be used instead", default="")
+    custom_scheme_prefix : bpy.props.StringProperty(name='Prefix', description="Custom prefix that will be used instead", default='')
 
     affected_side : bpy.props.EnumProperty(
         name="Affected side", 
